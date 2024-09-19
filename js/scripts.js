6a613e3178c878ae4a3fc5d12b81cb6671717ec7
John Danjuma <johnoluwashinaayomi@gmail.com>
2024-09-19 12:45:47 +0100

    flashcard

---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    $(".jav").mouseover(function() {
+        $(".java").slideToggle()
+        $(".html").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+      
+    });
+
+    $(".ht").mouseover(function() {
+        $(".html").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".cs").mouseover(function() {
+        $(".css").slideToggle()
+        $(".java").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+        $(".html").slideUp()
+    });
+    $(".met").mouseover(function() {
+        $(".method").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".arg").mouseover(function() {
+        $(".argue").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".stri").mouseover(function() {
+        $(".string").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".re").mouseover(function() {
+        $(".return").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".chi").mouseover(function() {
+        $(".chain").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".para").slideUp()
+        $(".play").slideUp()
+    });
+    $(".pa").mouseover(function() {
+        $(".para").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".play").slideUp()
+    });
+    $(".ply").mouseover(function() {
+        $(".play").slideToggle()
+        $(".java").slideUp()
+        $(".css").slideUp()
+        $(".method").slideUp()
+        $(".argue").slideUp()
+        $(".string").slideUp()
+        $(".return").slideUp()
+        $(".chain").slideUp()
+        $(".para").slideUp()
+    });
+});
+
